80dc289e872c5b6be9952d24f1da380eb986a97b
Shravan Sadalgekar <shravansadalgekar21105@gmail.com>
2026-04-25 23:00:10 +0530

    added root controller

---
+++ src/main/java/com/rcoem/devops/TestController.java
@@ -0,0 +1,13 @@
+package com.rcoem.devops;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String home() {
+        return "CI/CD SUCCESS 🔥";
+    }
+}
